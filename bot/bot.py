caab9afc3c9eb4bdc707579f6c301429107401d5
vsevolody <sevan4ik1337@Gmail.com>
2025-09-06 21:12:58 +0300

    Add aiogram to requirements

--- bot/bot.py
+++ bot/bot.py
@@ -1,25 +1,25 @@
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from aiogram.filters import Command
 import asyncio
 
 bot = Bot("7147939084:AAEg_yN3lj3tZZNEry5hFuL93e2oGEY6dD4")
 dp = Dispatcher()
 
-@dp.message(commands=["start"])
+@dp.message(Command("start"))
 async def start(message: types.Message):
-    kb = types.InlineKeyboardMarkup(
+    kb = InlineKeyboardMarkup(
         inline_keyboard=[
-            [types.InlineKeyboardButton(
+            [InlineKeyboardButton(
                 text="Открыть приложение",
-                web_app=types.WebAppInfo(url="https://tg-django-project.onrender.com/accounts/telegram-login/")
+                web_app=WebAppInfo(url="https://tg-django-project.onrender.com/accounts/telegram-login/")
             )]
         ]
     )
     await message.answer("Запусти мини-апп 👇", reply_markup=kb)
 
-
 async def main():
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())

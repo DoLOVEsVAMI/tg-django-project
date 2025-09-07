0306396fa597afbd5e8291be8f0e268cfd7c2f94
Alexandr Churkin <sanec527@mail.ru>
2025-09-07 15:58:57 +0300

    Fix aiogram v3 command handler

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
-    path("accounts/telegram-login/", telegram_login, name="telegram_login"),
+    path("telegram-login/", telegram_login, name="telegram_login"),
 ]
